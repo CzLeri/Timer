373b8cfeae5c9acd770faf378807b166386c85e8
CzLeri <v.leroch@gmail.com>
2020-04-07 00:13:59 +0200

    Created Timer

--- Timer/Timer/Form1.Designer.cs
+++ Timer/Timer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Timer
 {
-    partial class Form1
+    partial class fTimer
     {
         /// <summary>
         /// Vyžaduje se proměnná návrháře.
@@ -29,12 +29,73 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fTimer));
+            this.Menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIconTimer = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tHide = new System.Windows.Forms.Timer(this.components);
+            this.tBlock = new System.Windows.Forms.Timer(this.components);
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Menu
+            // 
+            this.Menu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Exit});
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(103, 28);
+            // 
+            // Exit
+            // 
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(102, 24);
+            this.Exit.Text = "Exit";
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // notifyIconTimer
+            // 
+            this.notifyIconTimer.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconTimer.Icon")));
+            this.notifyIconTimer.Text = "Timer";
+            this.notifyIconTimer.Visible = true;
+            // 
+            // tHide
+            // 
+            this.tHide.Enabled = true;
+            this.tHide.Interval = 1;
+            this.tHide.Tick += new System.EventHandler(this.tHide_Tick);
+            // 
+            // tBlock
+            // 
+            this.tBlock.Enabled = true;
+            this.tBlock.Interval = 300000;
+            this.tBlock.Tick += new System.EventHandler(this.tBlock_Tick);
+            // 
+            // fTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(0, 0);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(10000, 10000);
+            this.Name = "fTimer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Timer";
+            this.Menu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip Menu;
+        private System.Windows.Forms.ToolStripMenuItem Exit;
+        private System.Windows.Forms.NotifyIcon notifyIconTimer;
+        private System.Windows.Forms.Timer tHide;
+        private System.Windows.Forms.Timer tBlock;
     }
 }
 
--- Timer/Timer/Form1.cs
+++ Timer/Timer/Form1.cs
@@ -1,20 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace Timer
 {
-    public partial class Form1 : Form
+    public partial class fTimer : Form
     {
-        public Form1()
+        public fTimer()
         {
             InitializeComponent();
+            notifyIconTimer.ContextMenuStrip = this.Menu;
+        }
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern uint SetThreadExecutionState(EXECUTION_STATE esFlags);
+        private void KeepAlive()
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_SYSTEM_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void tHide_Tick(object sender, EventArgs e)
+        {
+            Hide();
+            tHide.Enabled = false;
+        }
+
+        private void tBlock_Tick(object sender, EventArgs e)
+        {
+            KeepAlive();
         }
     }
 }
